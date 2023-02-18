1be2e05e8836032ee5286c2eaa51c64c03bb5caa
Siyuan sharon Li <sharonsiyuan.li@mail.utoronto.ca>
2023-02-18 02:09:49 -0500

    added comment for login

--- src/welcome_page/LogIn.tsx
+++ src/welcome_page/LogIn.tsx
@@ -1,6 +1,11 @@
 import react, {useState} from "react";
 import {Page} from "../common";
 
+//SCENARIOS: 1. for newly signed up users, you are automaticaly logged in you do not need to use login function again
+//2. for newly signed up users, for testing, you can refresh the page and try log in with your credentials
+//3. NOTE: each user can only do one round of labelling, reason being python backend is not conencted yet to generate new round of email instructions
+//all user can only do one round, which is with the emails identified in day0.json, will implement later round logic when python backend connected
+//if a user attempt to do the second round of labelling, backend will crash and THIS IS EXPECTED
 function LogIn({ page, setPage, currentUser, setCurrentUser}: { page: number, setPage: (page: number) => void, currentUser: string, setCurrentUser: (currentUser: string) => void}) {
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
