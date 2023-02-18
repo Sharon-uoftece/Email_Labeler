af1d26f69158334ecfa79bc826950213bb9a8fc8
mhmchung <mhmchung@gmail.com>
2023-02-18 11:10:48 -0500

    added waiting for label logic and fixed some minor logic issues

--- pyscript/main.py
+++ pyscript/main.py
@@ -1,35 +1,36 @@
 import os
-import shutil
-from datetime import datetime
-import hashlib
+# import shutil
+# from datetime import datetime
+# import hashlib
 # from models import *
 import time
 import json
-  
+import models
+from models import expert_derived_ig_query_strategy
 
 
-JS_OUTPUT_DIR = './archived/temp/'
-ARCHIVED_DIR = './archived/processed/'
-LABEL_NUM_PER_SESSION = 10
+# JS_OUTPUT_DIR = './archived/temp/'
+# ARCHIVED_DIR = './archived/processed/'
+# LABEL_NUM_PER_SESSION = 10
 
-def move_files(path_to_source_file, filename):
-    if not os.path.exists(ARCHIVED_DIR):
-        os.makedirs(ARCHIVED_DIR)
+# def move_files(path_to_source_file, filename):
+#     if not os.path.exists(ARCHIVED_DIR):
+#         os.makedirs(ARCHIVED_DIR)
 
-    shutil.copy(path_to_source_file, f'{ARCHIVED_DIR}{filename}')
-    # os.rename(filename, './archived/processed/latest.txt')
+#     shutil.copy(path_to_source_file, f'{ARCHIVED_DIR}{filename}')
+#     # os.rename(filename, './archived/processed/latest.txt')
 
-def sha123(filename):
-    # read stuff in 64kb chunks!
-    BUF_SIZE = 65536  
-    sha1 = hashlib.sha1()
-    with open(filename, 'rb') as f:
-        while True:
-            data = f.read(BUF_SIZE)
-            if not data:
-                break
-            sha1.update(data)
-    return sha1.hexdigest()
+# def sha123(filename):
+#     # read stuff in 64kb chunks!
+#     BUF_SIZE = 65536  
+#     sha1 = hashlib.sha1()
+#     with open(filename, 'rb') as f:
+#         while True:
+#             data = f.read(BUF_SIZE)
+#             if not data:
+#                 break
+#             sha1.update(data)
+#     return sha1.hexdigest()
 
 
 def get_usernames():
@@ -57,12 +58,15 @@ if __name__ == "__main__":
             print(f'{r} is at {r} round(s)')
             if l == 10:
                 print("Start re-trainig ...")
-                # df_query_RB = main(user, "RB")
-                # df_query_EDIG = main(user, "EDIG")
-                # save_json(df_query_RB, df_query_EDIG)
-                # 
+                flag_RB, df_query_RB = models.main(user, "RB")
+                flag_EDIG, df_query_EDIG = models.main(user, "EDIG")
+                
+                if (flag_RB + flag_EDIG > 0):
+                    print("---waiting for labels from user {u}---".format(u=user))
+                else:
+                    models.save_json(user, df_query_RB, df_query_EDIG)
         
-        time.sleep(60*60)
+        time.sleep(5)
 
 
 # user = "c613d" # use this for testing
--- pyscript/models.py
+++ pyscript/models.py
@@ -11,8 +11,8 @@ from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 import json
-import glob
-import sys
+#import glob
+#import sys
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -31,7 +31,7 @@ def expert_derived_ig_query_strategy(classifier, X_pool, X_training, conf_traini
     beta = 0.9
     
     scores = alpha * uncertainty \
-             + alpha * beta * conf_weighted_distance_scores \
+             + alpha * beta * conf_weighted_similarity_scores \
              + (1 - alpha) * (1 - similarity_scores) * (1 - beta)
     
 
@@ -46,13 +46,13 @@ def read_json(data, day, model_type, var_name):
     return_arr = np.empty_like((5,), dtype=int) # get mid to be used for AL model teaching
     for row in data[str(day-1)]:
         if row['model_type'] == model_type:
-            return_arr = np.append(return_arr, row[var_name])
+            return_arr = np.append(return_arr, float(row[var_name]))
     return_arr = return_arr[1:]
     
     return return_arr
 
 
-def save_json(df_query_RB, df_query_EDIG):
+def save_json(user, df_query_RB, df_query_EDIG):
     # Shuffle
     df_query = pd.concat([df_query_RB, df_query_EDIG], 0).sample(frac=1).reset_index(drop=True)
     day = df_query['day'].values[0]
@@ -65,87 +65,91 @@ def save_json(df_query_RB, df_query_EDIG):
     parsed = json.loads(result)
 
     ### *** NEED TO MAKE SURE FORMATS MATCH !!! ***
-    with open(glob.glob('../history/{u}.json'.format(u=user))[0]) as json_file:
+    with open('../history/{u}.json'.format(u=user)) as json_file:
         json_decoded = json.load(json_file)
         
     json_decoded[str(day)] = parsed
     json_object = json.dumps(json_decoded, indent=4)
 
-    with open(glob.glob('../history/{u}.json'.format(u=user))[0], "w") as outfile:
+    with open('../history/{u}.json'.format(u=user), "w") as outfile:
         outfile.write(json_object)
 
 
 def main(user, model_type):
- 
+
     ### Read JS output (day N-1) ### 
-    with open(glob.glob('../history/{u}.json'.format(u=user))[0], 'r') as p: # read history of user
+    with open('../history/{u}.json'.format(u=user), 'r') as p: # read history of user
         data = json.load(p)
     
     day = int(max(data))+1 # find max day in history (.json) read
-    
-    mid = read_json(data, day, model_type, 'query_mid')
-    label = read_json(data, day, model_type, 'sensitive')
-    conf = read_json(data, day, model_type, 'confidence')
-        
-    
-    ### Load the model (day N-1) and pool data (day N-1) ###
-    if day>1:
-        learner = pickle.load(open('../model/{u}_learner_{m}_day{d}'.format(u=user,m=model_type,d=day-1), "rb"))
-        df_pool = pd.read_csv('../py_data/{u}_df_pool_{m}_day{d}.csv'.format(u=user,m=model_type,d=day-1))
-        X_pool = df_pool.loc[:, df_pool.columns!="target"]
-    else:
-        learner = pickle.load(open('../model/learner_{m}_day0'.format(m=model_type), "rb"))
-        df_pool = pd.read_csv('../py_data/df_pool_{m}_day0.csv'.format(m=model_type))
-        X_pool = df_pool.loc[:, df_pool.columns!="target"]
-        
-        
-    
-    
-    ### Teach the model ###
-    idx = X_pool.index[X_pool.mid.isin(mid)]
-    learner.teach(X=X_pool.values[idx], y=pd.Series(label).values)
-    
-    
-    ### Update labeled data (mainly for EDIG training) ###
-    if day>1:
-        df_labeled = pd.read_csv('../py_data/{u}_df_labeled_{m}.csv'.format(u=user,m=model_type))
-    else:
-        df_labeled = pd.read_csv('../py_data/df_labeled_{m}.csv'.format(m=model_type))                             
-                             
-    df_temp = X_pool[X_pool.mid.isin(mid)]
-    df_temp['label'] = label
-    df_temp['conf'] = conf
-    df_labeled = pd.concat([df_labeled, df_temp], 0)
-    
 
-    ### Update pool data (remove mid from pool day N in case of duplication in day N+1) ###
-    df_pool = df_pool[~df_pool.mid.isin(idx)].iloc[:, df_pool.columns!="target"]
+    if (['sensitive'] not in data[str(day-1)]):
+        return 1, []
     
-    
-    ### Generate query (new 5 ins) for each model
-    conf_training = df_labeled["conf"]
-    X_training = df_labeled.iloc[:, :-2]
-    
-    if model_type == "EDIG":
-        query_idx, query_ins = learner.query(df_pool.values, X_training.values, conf_training.values)
-    else:
-        query_idx, query_ins = learner.query(df_pool.values)
+    else:        
+        mid = read_json(data, day, model_type, 'query_mid')
+        label = read_json(data, day, model_type, 'sensitive')
+        conf = read_json(data, day, model_type, 'confidence')
+            
         
-    query_mid = df_pool["mid"].iloc[query_idx]
-    
-
-    ### Save CSVs and model ###
-    pickle.dump(learner, open('../model/{u}_learner_{m}_day{d}'.format(u=user,m=model_type,d=day), "wb"))
-    df_pool.to_csv('../py_data/{u}_df_pool_{m}_day{d}.csv'.format(u=user,m=model_type,d=day), index=False)
-    df_labeled.to_csv('../py_data/{u}_df_labeled_{m}.csv'.format(u=user,m=model_type), index=False)
+        ### Load the model (day N-1) and pool data (day N-1) ###
+        if day>1:
+            learner = pickle.load(open('../model/{u}_learner_{m}_day{d}'.format(u=user,m=model_type,d=day-1), "rb"))
+            df_pool = pd.read_csv('../py_data/{u}_df_pool_{m}_day{d}.csv'.format(u=user,m=model_type,d=day-1))
+            X_pool = df_pool.loc[:, df_pool.columns!="target"]
+        else:
+            learner = pickle.load(open('../model/learner_{m}_day0'.format(m=model_type), "rb"))
+            df_pool = pd.read_csv('../py_data/df_pool_{m}_day0.csv'.format(m=model_type))
+            X_pool = df_pool.loc[:, df_pool.columns!="target"]
+            
+            
+        
+        
+        ### Teach the model ###
+        idx = X_pool.index[X_pool.mid.isin(mid)]
+        learner.teach(X=X_pool.values[idx], y=pd.Series(label).values)
+        
+        
+        ### Update labeled data (mainly for EDIG training) ###
+        if day>1:
+            df_labeled = pd.read_csv('../py_data/{u}_df_labeled_{m}.csv'.format(u=user,m=model_type))
+        else:
+            df_labeled = pd.read_csv('../py_data/df_labeled_{m}.csv'.format(m=model_type))                             
+                                 
+        df_temp = X_pool[X_pool.mid.isin(mid)]
+        df_temp['label'] = label
+        df_temp['conf'] = conf
+        df_labeled = pd.concat([df_labeled, df_temp], 0)
         
     
-    ### Return query
-    df_query = pd.DataFrame(query_mid.values, columns=["query_mid"]).reset_index(drop=True)
-    df_query["day"] = day
-    df_query["model_type"] = model_type
+        ### Update pool data (remove mid from pool day N in case of duplication in day N+1) ###
+        df_pool = df_pool[~df_pool.mid.isin(idx)].iloc[:, df_pool.columns!="target"]
+        
+        
+        ### Generate query (new 5 ins) for each model
+        conf_training = df_labeled["conf"]
+        X_training = df_labeled.iloc[:, :-2]
+        
+        if model_type == "EDIG":
+            query_idx, query_ins = learner.query(df_pool.values, X_training.values, conf_training.values)
+        else:
+            query_idx, query_ins = learner.query(df_pool.values)
+            
+        query_mid = df_pool["mid"].iloc[query_idx]
+        
     
-    return df_query
+        ### Save CSVs and model ###
+        pickle.dump(learner, open('../model/{u}_learner_{m}_day{d}'.format(u=user,m=model_type,d=day), "wb"))
+        df_pool.to_csv('../py_data/{u}_df_pool_{m}_day{d}.csv'.format(u=user,m=model_type,d=day), index=False)
+        df_labeled.to_csv('../py_data/{u}_df_labeled_{m}.csv'.format(u=user,m=model_type), index=False)
+            
+        
+        ### Return query
+        df_query = pd.DataFrame(query_mid.values, columns=["query_mid"]).reset_index(drop=True)
+        df_query["day"] = day
+        df_query["model_type"] = model_type
+        
+        return 0, df_query
 
     
 ############
